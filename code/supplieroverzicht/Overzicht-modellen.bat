0c01396ad433f0d3dbbffe38576aaeb4d3378cce
melsk-r <robert.melskens@vng.nl>
2023-03-01 14:18:02 +0100

    Update Overzicht-modellen.bat

--- code/supplieroverzicht/Overzicht-modellen.bat
+++ code/supplieroverzicht/Overzicht-modellen.bat
@@ -7,10 +7,6 @@ set basefolder=C:\Data\Temp\GitHub\Model-Driven-Design-Documentatie\code\supplie
 
 C:
 cd %trunk%
-del SIMcatalogue.txt
-del UGMcatalogue.txt
-del BSMcatalogue.txt
-
 cd SIM
 dir /B > ../SIMcatalogue.txt
 cd ..\UGM
@@ -19,10 +15,6 @@ cd ..\BSM
 dir /B > ../BSMcatalogue.txt
 
 cd ..\..\branches
-del SIMcatalogue.txt
-del UGMcatalogue.txt
-del BSMcatalogue.txt
-
 cd SIM
 dir /B > ../SIMcatalogue.txt
 cd ..\UGM
@@ -31,9 +23,6 @@ cd ..\BSM
 dir /B > ../BSMcatalogue.txt
 
 cd ..\..\tags
-del SIMcatalogue.txt
-del UGMcatalogue.txt
-del BSMcatalogue.txt
 cd SIM
 dir /B > ../SIMcatalogue.txt
 cd ..\UGM
@@ -41,9 +30,23 @@ dir /B > ../UGMcatalogue.txt
 cd ..\BSM
 dir /B > ../BSMcatalogue.txt
 
-cls
-
 cd %basefolder%
 
 rem java -Xmx1024m -jar %saxon_path% -o:Supplieroverzicht.html -s:Supplieroverzicht-html.xsl -xsl:Supplieroverzicht-html.xsl SVNpath=file:////C:/Data/KING/Kern-taken/StUF/EAP-bronnen/XMI-data/
 java -Xmx1024m -cp %saxon11_path% net.sf.saxon.Transform -t -o:Supplieroverzicht.html -s:Supplieroverzicht-html.xsl -xsl:Supplieroverzicht-html.xsl SVNpath=file:////C:/Data/KING/Kern-taken/StUF/EAP-bronnen/XMI-data/
+
+cd %trunk%
+del SIMcatalogue.txt
+del UGMcatalogue.txt
+del BSMcatalogue.txt
+
+cd ..\branches
+del SIMcatalogue.txt
+del UGMcatalogue.txt
+del BSMcatalogue.txt
+
+
+cd ..\tags
+del SIMcatalogue.txt
+del UGMcatalogue.txt
+del BSMcatalogue.txt
